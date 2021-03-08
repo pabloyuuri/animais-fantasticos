e8171565608fe3d8e59eb77972161f10eae16b39
Pablo <pabloyuuri@gmail.com>
2021-03-08 10:02:31 -0300

    Accordion transformado em Classe Object

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import iniTooltip from './modules/tooltip.js';
 import initModal from './modules/modal.js';
@@ -13,11 +13,13 @@ import initFetchBitcoin from './modules/fetch-bitcoin.js';
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const accordionList = new Accordion('[data-menu="accordion] dt');
+accordionList.init();
+
 initFuncionamento();
 initMenuMobile();
 initModal();
 iniTooltip();
-initAccordion();
 initTabNav();
 initDropDownMenu();
 initFetchAnimais();
