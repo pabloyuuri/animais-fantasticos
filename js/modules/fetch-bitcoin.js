e0925d310b7baf5bd5229b99547b5750b7f6f514
Pablo <pabloyuuri@gmail.com>
2021-03-08 23:31:26 -0300

    fetch-bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,8 +1,8 @@
-export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then((response) => response.json())
     .then((json) => {
-      const spanValor = document.querySelector('.btc-preco');
+      const spanValor = document.querySelector(target);
       spanValor.innerText = (1000 / json.BRL.sell).toFixed(4);
     }).catch((error) => {
       console.log(Error(error));
--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ import initDropDownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -28,7 +28,7 @@ tooltip.init();
 initFuncionamento();
 initMenuMobile();
 initDropDownMenu();
-initFetchBitcoin();
 initAnimacaoScroll();
 
 fetchAnimais('./js/animaisapi.json', '.numeros-grid');
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
